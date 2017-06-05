f3bf06be23e5cd65668aab7d3d9a9d173b27d461
KhaozFrost <KhaozFrost@gmail.com>
2017-06-04 22:17:30 -0400

    Increase Pokeball Rewards

--- src/main/java/com/xpgaming/xPHunt/Enums/EnumItems.java
+++ src/main/java/com/xpgaming/xPHunt/Enums/EnumItems.java
@@ -1,13 +1,25 @@
 package com.xpgaming.xPHunt.Enums;
 
 public enum EnumItems {
+        level_ball,
+        moon_ball,
+        friend_ball,
+        love_ball,
+        timer_ball,
+        nest_ball,
+        net_ball,
+        dive_ball,
+        luxury_ball,
+        heal_ball,
+        dusk_ball,
+        lure_ball,
+        sport_ball,
         ultra_ball,
         great_ball,
         quick_ball,
         heavy_ball,
         fast_ball,
-        repeat_ball,
-        gs_ball;
+        repeat_ball;
 
         private EnumItems() {
         }
